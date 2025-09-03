26f60a5ee787cfe9af15b5d69a964fbabb91728d
Ömer Üçüncü <118854548+omerrucuncu@users.noreply.github.com>
2025-09-03 17:39:27 +0300

    Add project files.

---
+++ LearnerProject/Models/Entities/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnerProject.Models.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Icon { get; set; }
+        public bool Status { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ LearnerProject/Models/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnerProject.Models.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
