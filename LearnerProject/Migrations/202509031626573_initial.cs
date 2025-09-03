c58b730758acadda8c75f94c0beda48f9b348971
Ömer Üçüncü <118854548+omerrucuncu@users.noreply.github.com>
2025-09-03 20:03:26 +0300

    Add Entity Framework 6.5.1 and update database schema
    Updated `LearnerProject.csproj` to reference Entity Framework 6.5.1 and added necessary imports. Created migration files and a configuration class for database migrations. Modified `Web.config` to include Entity Framework settings and connection strings. Updated `packages.config` to include the new Entity Framework package. Added migration classes to define the database schema for entities like Students, Courses, and Categories. Enhanced `LearnerContext` with DbSet properties for these entities.

---
+++ LearnerProject/Migrations/202509031626573_initial.cs
@@ -0,0 +1,267 @@
+﻿namespace LearnerProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Abouts",
+                c => new
+                    {
+                        AboutId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        ImageUrl = c.String(),
+                        Item1 = c.String(),
+                        Item2 = c.String(),
+                        Item3 = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.AboutId);
+            
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        AdminId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        ImageUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.AdminId);
+            
+            CreateTable(
+                "dbo.Banners",
+                c => new
+                    {
+                        BannerId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                    })
+                .PrimaryKey(t => t.BannerId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        CategoryName = c.String(),
+                        Icon = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        CourseId = c.Int(nullable: false, identity: true),
+                        CourseName = c.String(),
+                        ImageUrl = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CategoryId = c.Int(nullable: false),
+                        ClubId = c.Int(nullable: false),
+                        Teacher_TeacherId = c.Int(),
+                    })
+                .PrimaryKey(t => t.CourseId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.Clubs", t => t.ClubId, cascadeDelete: true)
+                .ForeignKey("dbo.Teachers", t => t.Teacher_TeacherId)
+                .Index(t => t.CategoryId)
+                .Index(t => t.ClubId)
+                .Index(t => t.Teacher_TeacherId);
+            
+            CreateTable(
+                "dbo.Clubs",
+                c => new
+                    {
+                        ClubId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Icon = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.ClubId);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        ContactId = c.Int(nullable: false, identity: true),
+                        Address = c.String(),
+                        OpenHours = c.String(),
+                        Email = c.String(),
+                        Phone = c.String(),
+                    })
+                .PrimaryKey(t => t.ContactId);
+            
+            CreateTable(
+                "dbo.CourseRegisters",
+                c => new
+                    {
+                        CourseRegisterId = c.Int(nullable: false, identity: true),
+                        StudentId = c.Int(nullable: false),
+                        CourseId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CourseRegisterId)
+                .ForeignKey("dbo.Courses", t => t.CourseId, cascadeDelete: true)
+                .ForeignKey("dbo.Students", t => t.StudentId, cascadeDelete: true)
+                .Index(t => t.StudentId)
+                .Index(t => t.CourseId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        UserName = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.StudentId);
+            
+            CreateTable(
+                "dbo.Reviews",
+                c => new
+                    {
+                        ReviewId = c.Int(nullable: false, identity: true),
+                        CourseId = c.Int(nullable: false),
+                        ReviewValue = c.Double(nullable: false),
+                        Comment = c.String(),
+                        StudentId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ReviewId)
+                .ForeignKey("dbo.Courses", t => t.CourseId, cascadeDelete: true)
+                .ForeignKey("dbo.Students", t => t.StudentId, cascadeDelete: true)
+                .Index(t => t.CourseId)
+                .Index(t => t.StudentId);
+            
+            CreateTable(
+                "dbo.CourseVideos",
+                c => new
+                    {
+                        CourseVideoId = c.Int(nullable: false, identity: true),
+                        CourseId = c.Int(nullable: false),
+                        VideoNumber = c.Int(nullable: false),
+                        VideoUrl = c.String(),
+                        TeacherId = c.Int(),
+                    })
+                .PrimaryKey(t => t.CourseVideoId)
+                .ForeignKey("dbo.Courses", t => t.CourseId, cascadeDelete: true)
+                .ForeignKey("dbo.Teachers", t => t.TeacherId)
+                .Index(t => t.CourseId)
+                .Index(t => t.TeacherId);
+            
+            CreateTable(
+                "dbo.Teachers",
+                c => new
+                    {
+                        TeacherId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        ImageUrl = c.String(),
+                        Title = c.String(),
+                        Graduate = c.String(),
+                        Experience = c.String(),
+                        ExperienceYear = c.String(),
+                        Skill = c.String(),
+                        Certificate = c.String(),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.TeacherId);
+            
+            CreateTable(
+                "dbo.FAQs",
+                c => new
+                    {
+                        FAQId = c.Int(nullable: false, identity: true),
+                        Question = c.String(),
+                        Answer = c.String(),
+                        ImageUrl = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.FAQId);
+            
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        MessageId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        Email = c.String(),
+                        Subject = c.String(),
+                        MessageContent = c.String(),
+                        IsRead = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.MessageId);
+            
+            CreateTable(
+                "dbo.SocialMedias",
+                c => new
+                    {
+                        SocialMediaId = c.Int(nullable: false, identity: true),
+                        SocialMediaName = c.String(),
+                        SocialMediaUrl = c.String(),
+                        Icon = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.SocialMediaId);
+            
+            CreateTable(
+                "dbo.Testimonials",
+                c => new
+                    {
+                        TestimonialId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        Title = c.String(),
+                        ImageUrl = c.String(),
+                        Comment = c.String(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.TestimonialId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CourseVideos", "TeacherId", "dbo.Teachers");
+            DropForeignKey("dbo.Courses", "Teacher_TeacherId", "dbo.Teachers");
+            DropForeignKey("dbo.CourseVideos", "CourseId", "dbo.Courses");
+            DropForeignKey("dbo.Reviews", "StudentId", "dbo.Students");
+            DropForeignKey("dbo.Reviews", "CourseId", "dbo.Courses");
+            DropForeignKey("dbo.CourseRegisters", "StudentId", "dbo.Students");
+            DropForeignKey("dbo.CourseRegisters", "CourseId", "dbo.Courses");
+            DropForeignKey("dbo.Courses", "ClubId", "dbo.Clubs");
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.CourseVideos", new[] { "TeacherId" });
+            DropIndex("dbo.CourseVideos", new[] { "CourseId" });
+            DropIndex("dbo.Reviews", new[] { "StudentId" });
+            DropIndex("dbo.Reviews", new[] { "CourseId" });
+            DropIndex("dbo.CourseRegisters", new[] { "CourseId" });
+            DropIndex("dbo.CourseRegisters", new[] { "StudentId" });
+            DropIndex("dbo.Courses", new[] { "Teacher_TeacherId" });
+            DropIndex("dbo.Courses", new[] { "ClubId" });
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropTable("dbo.Testimonials");
+            DropTable("dbo.SocialMedias");
+            DropTable("dbo.Messages");
+            DropTable("dbo.FAQs");
+            DropTable("dbo.Teachers");
+            DropTable("dbo.CourseVideos");
+            DropTable("dbo.Reviews");
+            DropTable("dbo.Students");
+            DropTable("dbo.CourseRegisters");
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Clubs");
+            DropTable("dbo.Courses");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Banners");
+            DropTable("dbo.Admins");
+            DropTable("dbo.Abouts");
+        }
+    }
+}
---
+++ LearnerProject/Models/Context/LearnerContext.cs
@@ -0,0 +1,34 @@
+﻿using LearnerProject.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LearnerProject.Models.Context
+{
+    public class LearnerContext : DbContext
+    {
+        // Bütün sınıflar için "DbSet" tanımlamaları burada yapılır.
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<CourseRegister> CourseRegisters { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<FAQ> FAQs { get; set; }
+        public DbSet<CourseVideo> CourseVideos { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<About> Abouts { get; set; }
+
+
+
+
+    }
+}
